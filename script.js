f1155087fcaff8042bab72560ad677211ff91252
danielbosa <danielbosa95@gmail.com>
2024-03-08 15:44:37 +0100

    JS Cycle: for To: print FizzBuzz if let i divisbile per 3 & 5; Fizz if divisibile per 3; Buzz if divisible per 5; i elsewhere

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 
-for (let i = 0; i < 100; i++){
-    console.log (i + 1);
+for (let i = 1; i < 100; i++){
+    if ((i % 3 == 0) && (i % 5 == 0))
+    console.log('FizzBuzz');
+    else if (i % 3 == 0)
+    console.log('Fizz');
+    else if (i % 5 == 0)
+    console.log('Buzz');
+    else
+    console.log (i);
 };
